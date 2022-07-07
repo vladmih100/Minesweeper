a71af087f28f823af6f5516f92f14cccb99e1744
Mihnea Vlad <daomag@gmail.com>
2022-07-08 01:07:03 +1200

    Finished adding frames to interface

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from tkinter import *
+import settings
+import utilities
+
 
 ### Initialising window ###
 root = Tk() # Creates window
 root.configure(bg='black') # Set background colour
-root.geometry('1000x500') # Sets window size
+root.geometry(f'{settings.width}x{settings.height}') # Sets window size
 root.title('Minesweeper') # Sets window title
 root.resizable(False, False) # Prevents resizing of window
 
 ### Creating Frames ###
+topFrame = Frame(
+    root,
+    bg='red',
+    width = utilities.width_prct(100),
+    height = utilities.height_prct(25)
+)
 
+topFrame.place(x=0, y=0)
+
+leftFrame = Frame(
+    root,
+    bg='blue',
+    width = utilities.width_prct(25), 
+    height = utilities.height_prct(75)
+)
+
+leftFrame.place(x=0, y=utilities.height_prct(25))
+
+centreFrame = Frame(
+    root,
+    bg='green',
+    width = utilities.width_prct(75), 
+    height = utilities.height_prct(75)
+)
+
+centreFrame.place(x=utilities.width_prct(25), y=utilities.height_prct(25))
 
 root.mainloop() # Runs window until closed by user
---
+++ utilities.py
@@ -0,0 +1,7 @@
+import settings
+
+def height_prct(percentage):
+    return settings.height * percentage/100
+
+def width_prct(percentage):
+    return settings.width * percentage/100
