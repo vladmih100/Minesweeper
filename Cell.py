da90a6f7d8051c35c4f21158db7df51a357d2bdc
Mihnea Vlad <daomag@gmail.com>
2022-07-12 12:52:06 +1200

    Added right click action

--- Cell.py
+++ Cell.py
@@ -1,17 +1,25 @@
 from tkinter import Button
 
+# Creating cell object
 class Cell:
     def __init__(self, Mine=False):
         self.Mine = Mine
         self.btnObject = None
 
+    # Assigns TKinter button to cell object
     def createButton(self, location):
         btn = Button(
             location,
             text='Hi'
         )
-        btn.bind('<Button-1>', self.leftClick)
+        btn.bind('<Button-1>', self.leftClick) # Left click action
+        btn.bind('<Button-3>', self.rightClick) # Roght click action
         self.btnObject = btn
 
     def leftClick(self, event):
+        print(event)
         print("I am left clicked")
+
+    def rightClick(self, event):
+        print(event)
+        print("I am right clicked")
