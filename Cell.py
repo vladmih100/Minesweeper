51ad4d4475a7ed5d8fdff276f8fbc2b5a511c357
Mihnea Vlad <daomag@gmail.com>
2022-07-13 02:27:35 +1200

    Wrote functions to display number of mines surrounding a cell

--- Cell.py
+++ Cell.py
@@ -1,4 +1,5 @@
 from tkinter import Button
+import settings
 import random
 
 # Creating cell object
@@ -18,15 +19,55 @@ class Cell:
             location,
             width=10,
             height=3,
-            text=f"{self.x},{self.y}"
         )
         btn.bind('<Button-1>', self.leftClick) # Left click action
         btn.bind('<Button-3>', self.rightClick) # Roght click action
         self.btnObject = btn
 
+    # Left click actions
     def leftClick(self, event):
-        print(event)
-        print("I am left clicked")
+        # If clicked cell is mine, cell turns red:
+        if self.Mine: 
+            self.showMine()
+        else:
+            self.showCell()
+
+    def showMine(self):
+        self.btnObject.configure(bg='red')
+
+    def getCell(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    # Returns array of surrounding cell objects
+    @property
+    def surroundedCells(self):
+        Cells = [
+            self.getCell(self.x - 1, self.y - 1),
+            self.getCell(self.x - 1, self.y),
+            self.getCell(self.x - 1, self.y + 1),
+            self.getCell(self.x, self.y - 1),
+            self.getCell(self.x, self.y + 1),
+            self.getCell(self.x + 1, self.y - 1),
+            self.getCell(self.x + 1, self.y),
+            self.getCell(self.x + 1, self.y + 1),
+        ]
+        Cells = [cell for cell in Cells if cell != None]
+        return Cells
+
+    # Counts how many mines surronding selected cell
+    @property
+    def surroundedMines(self):
+        count = 0
+        for cell in self.surroundedCells:
+            if cell.Mine:
+                count += 1
+
+        return count
+
+    def showCell(self):
+        self.btnObject.configure(text=self.surroundedMines)
 
     def rightClick(self, event):
         print(event)
@@ -36,7 +77,7 @@ class Cell:
     def randomiseMines():
         pickedCells = random.sample(
             Cell.all,
-            9
+            settings.mineCount
         )
 
         for pickedCell in pickedCells:
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 width = 1000
 height = 500
 gridSize = 6
+mineCount = (gridSize ** 2) // 4
